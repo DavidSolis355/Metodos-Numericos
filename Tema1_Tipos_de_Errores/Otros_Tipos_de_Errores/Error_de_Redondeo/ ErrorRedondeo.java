5bbefd2f2f3a7c30051767451872e37cb940a7b7
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-25 21:51:16 -0600

    ErrorRedondeo Codigo Parte 5

--- Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Redondeo/ ErrorRedondeo.java
+++ Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Redondeo/ ErrorRedondeo.java
@@ -16,7 +16,15 @@ public class ErrorRedondeo {
     public static void main(String[] args) {
         double valorOriginal = 3.14159265;
         int decimales = 4;
+
+        double valorRedondeado = redondear(valorOriginal, decimales);
+        double errorRedondeo = calcularErrorRedondeo(valorOriginal, valorRedondeado);
+
+        System.out.println("Valor original: " + valorOriginal);
+        System.out.println("Valor redondeado: " + valorRedondeado);
+        System.out.println("Error de Redondeo: " + errorRedondeo);
     }
 
+    
 
 }
