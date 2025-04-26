9211837787707acf096ce6fc0b32d962976ef2fa
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 16:01:47 -0600

    DiferenciacionTresPuntos Codigo Parte 2

--- Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Diferenciacion_Numerica/Metodo_de_los_3_Puntos/DiferenciacionTresPuntos.java
+++ Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Diferenciacion_Numerica/Metodo_de_los_3_Puntos/DiferenciacionTresPuntos.java
@@ -1,4 +1,10 @@
 public class DiferenciacionTresPuntos {
-    
 
+    public static void main(String[] args) {
+
+    }
+
+    public static double derivadaTresPuntos(double[] puntos, double h) {
+        
+    }
 }
