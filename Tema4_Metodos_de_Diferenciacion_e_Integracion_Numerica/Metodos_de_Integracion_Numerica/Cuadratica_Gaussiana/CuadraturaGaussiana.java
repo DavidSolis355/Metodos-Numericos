4cd79b4bc1047e348406b10c2de5d66f4c85ddb1
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 16:16:20 -0600

    CuadraturaGaussiana Codigo Parte 3

--- Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Cuadratica_Gaussiana/CuadraturaGaussiana.java
+++ Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Cuadratica_Gaussiana/CuadraturaGaussiana.java
@@ -4,7 +4,26 @@ import java.util.function.Function;
 public class CuadraturaGaussiana {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("INTEGRACIÓN NUMÉRICA (CUADRATURA GAUSSIANA)");
+        System.out.println("---------------------------------------------");
+        
+        System.out.println("Introduzca la función (use 'x' como variable, ej: exp(x)*cos(x)): ");
+        String funcionStr = scanner.nextLine();
+        Function<Double, Double> f = x -> evaluarFuncion(funcionStr.replace("x", String.valueOf(x)));
+        
+        System.out.println("Introduzca el límite inferior de integración (a): ");
+        double a = scanner.nextDouble();
+        
+        System.out.println("Introduzca el límite superior de integración (b): ");
+        double b = scanner.nextDouble();
+        
+        double integral = gauss2Puntos(f, a, b);
+        
+        System.out.println("\nRESULTADO:");
+        System.out.printf("La integral de f(x) en el intervalo [%.4f, %.4f] ≈ %.12f%n", a, b, integral);
+        System.out.println("Fórmula utilizada: Cuadratura Gaussiana de 2 puntos");
     }
 
     private static double gauss2Puntos(Function<Double, Double> f, double a, double b) {
@@ -15,6 +34,6 @@ public class CuadraturaGaussiana {
 
     }
 
-    
+
 
 }
