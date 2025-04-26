5b6275fbf119c53ffc383607460ad1fa6e18145c
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 13:49:55 -0600

    Jacobi Codigo Parte 2

--- Tema3_Metodos_para_Sistemas_de_Ecuaciones_Lineales/Metodos_Iterativos/Metodo_de_Jacobi/Jacobi.java
+++ Tema3_Metodos_para_Sistemas_de_Ecuaciones_Lineales/Metodos_Iterativos/Metodo_de_Jacobi/Jacobi.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Jacobi {
-    
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double[][] generarMatriz(int orden) {
+
+    }
+
+    public static void mostrarMatriz(double[][] matriz, int orden) {
+        
+    }
 }
+
+
