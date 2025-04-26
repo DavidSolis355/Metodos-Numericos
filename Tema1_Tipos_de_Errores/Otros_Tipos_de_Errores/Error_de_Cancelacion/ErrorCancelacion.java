82e84f538da803bd012056b60de8a4127dd1b3e5
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-25 21:28:15 -0600

    ErrorCancelacion Codigo Parte 3

--- Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Cancelacion/ErrorCancelacion.java
+++ Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Cancelacion/ErrorCancelacion.java
@@ -1,5 +1,16 @@
 public class ErrorCancelacion {
 
+    public static void calcularCancelacion(double a, double b) {
+        double resultadoExacto = a - b;
+        double resultadoAproximado = (float) a - (float) b; // Simulamos pérdida de precisión
+
+        double errorCancelacion = Math.abs(resultadoExacto - resultadoAproximado);
+
+        System.out.println("Resultado Exacto (double): " + resultadoExacto);
+        System.out.println("Resultado Aproximado (float): " + resultadoAproximado);
+        System.out.println("Error por Cancelación: " + errorCancelacion);
+    }
+
     public static void main(String[] args) {
         double a = 1234567.89;
         double b = 1234567.88;
