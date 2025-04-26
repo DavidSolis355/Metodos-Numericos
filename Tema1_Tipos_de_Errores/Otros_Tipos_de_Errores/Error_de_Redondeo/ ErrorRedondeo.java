8899e572182bcc8d6aea84820b413c3e666fdcdd
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-25 21:49:03 -0600

    ErrorRedondeo Codigo Parte 2

--- Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Redondeo/ ErrorRedondeo.java
+++ Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Redondeo/ ErrorRedondeo.java
@@ -1,3 +1,12 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class ErrorRedondeo {
-    
+
+    public static void main(String[] args) {
+        double valorOriginal = 3.14159265;
+        int decimales = 4;
+    }
+
+
 }
