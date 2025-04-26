8f4503bd93d5dfcb7c561bd6fa21aee5a8373616
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 16:23:12 -0600

    ReglaDelTrapecio Codigo Parte 2

--- Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Regla_del_Trapecio/ReglaDelTrapecio.java
+++ Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Regla_del_Trapecio/ReglaDelTrapecio.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class ReglaDelTrapecio {
-    
+
+    public static void main(String[] args) {
+
+    }
+
+    private static double trapecio(Function<Double, Double> f, double a, double b, int n) {
+
+    }
+
+    private static double evaluarFuncion(String expr) {
+
+    }
+
+
 }
