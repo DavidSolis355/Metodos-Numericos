39b3a927b6fd76dd470fcee383fa23dbd525f671
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 16:15:16 -0600

    CuadraturaGaussiana Codigo Parte 2

--- Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Cuadratica_Gaussiana/CuadraturaGaussiana.java
+++ Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Cuadratica_Gaussiana/CuadraturaGaussiana.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class CuadraturaGaussiana {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static double gauss2Puntos(Function<Double, Double> f, double a, double b) {
+
+    }
+
+    private static double evaluarFuncion(String expr) {
+
+    }
+
     
+
 }
