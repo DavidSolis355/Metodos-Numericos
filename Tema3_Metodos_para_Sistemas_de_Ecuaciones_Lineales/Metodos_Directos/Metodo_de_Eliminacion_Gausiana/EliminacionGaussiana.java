c62ceaee9bcc00e31c77d6997e7ba898d53ab0b5
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 12:48:10 -0600

    EliminacionGaussiana Codigo Parte 3

--- Tema3_Metodos_para_Sistemas_de_Ecuaciones_Lineales/Metodos_Directos/Metodo_de_Eliminacion_Gausiana/EliminacionGaussiana.java
+++ Tema3_Metodos_para_Sistemas_de_Ecuaciones_Lineales/Metodos_Directos/Metodo_de_Eliminacion_Gausiana/EliminacionGaussiana.java
@@ -56,13 +56,23 @@ public class EliminacionGaussiana
 
     public static double[][] generarMatriz(int orden)
     {
-
-
+        Random rand = new Random();
+        double[][] matriz = new double[orden][orden+1];
+        
+        for(int i = 0; i < orden; i++)
+        {
+            for (int j = 0; j < orden + 1; j ++)
+            {
+                matriz[i][j] = rand.nextInt(10) - 5;
+            }
+        }
+        
+        return matriz;
     }
 
     public static void mostrarMatriz(double[][] matriz, int orden)
     {
-        
+
     }
 
 
