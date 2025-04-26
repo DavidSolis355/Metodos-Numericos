b86ed5b954d01ed98d6d3710025fb98ecaf2edf4
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 16:08:43 -0600

    Diferenciacion5Puntos Codigo Parte 2

--- Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Diferenciacion_Numerica/Metodo_de_los_5_Puntos/Diferenciacion5Puntos.java
+++ Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Diferenciacion_Numerica/Metodo_de_los_5_Puntos/Diferenciacion5Puntos.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class Diferenciacion5Puntos {
-    
+
+    public static void main(String[] args) {
+
+    }
+
+    private static double evaluarFuncion(String expr) {
+
+    }
+
 }
