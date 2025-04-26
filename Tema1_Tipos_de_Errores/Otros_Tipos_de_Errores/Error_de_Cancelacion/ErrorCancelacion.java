ff8ddafc429faa5668bf2a05036bd1e5dac99c17
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-25 21:28:49 -0600

    ErrorCancelacion Codigo Parte 4 (Terminado)

--- Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Cancelacion/ErrorCancelacion.java
+++ Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Cancelacion/ErrorCancelacion.java
@@ -14,6 +14,9 @@ public class ErrorCancelacion {
     public static void main(String[] args) {
         double a = 1234567.89;
         double b = 1234567.88;
+
+        calcularCancelacion(a, b);
+
     }
 
 }
