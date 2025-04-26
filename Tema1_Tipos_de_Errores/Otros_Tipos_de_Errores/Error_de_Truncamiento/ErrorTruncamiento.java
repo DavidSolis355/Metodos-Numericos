4a7f56cb64d76fcb126e3811a857f0f81d9c49bc
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-25 22:44:12 -0600

    ErrorTruncamiento Codigo Parte 2

--- Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Truncamiento/ErrorTruncamiento.java
+++ Tema1_Tipos_de_Errores/Otros_Tipos_de_Errores/Error_de_Truncamiento/ErrorTruncamiento.java
@@ -1,8 +1,13 @@
 public class ErrorTruncamiento {
 
+    public static double truncar(double valor, int decimales) {
+        double factor = Math.pow(10, decimales);
+        return Math.floor(valor * factor) / factor;
+    }
+
     public static void main(String[] args) {
         double valorOriginal = 3.14159265;
         int decimales = 4;
-        
+
     }
 }
