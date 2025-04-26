5e51e67ab57c3d2100232448743804528e011e7f
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 16:29:06 -0600

    Simpson13 Codigo Parte 2

--- Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Regla_de_Simpson_1\3/Simpson13.java
+++ Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Regla_de_Simpson_1\3/Simpson13.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class Simpson13 {
-    
+
+    public static void main(String[] args) {
+
+    }
+
+    private static double simpson13(Function<Double, Double> f, double a, double b, int n) {
+
+    }
+
+    private static double evaluarFuncion(String expr) {
+        
+    }
 }
