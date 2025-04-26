f65631580f9b489e84bc7629a6cd893b7562e095
DavidSolis355 <23030006@itesa.edu.mx>
2025-04-26 16:35:54 -0600

    Simpson38 Codigo Parte 2

--- Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Regla_de_Simpson_3\8/Simpson38.java
+++ Tema4_Metodos_de_Diferenciacion_e_Integracion_Numerica/Metodos_de_Integracion_Numerica/Regla_de_Simpson_3\8/Simpson38.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class Simpson38 {
-    
+
+    public static void main(String[] args) {
+
+    }
+
+    private static double simpson38(Function<Double, Double> f, double a, double b, int n) {
+
+    }
+
+    private static double evaluarFuncion(String expr) {
+        
+    }
+
+
 }
