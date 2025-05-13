bcc5af190a33cee476cbec4d9ae88225151cf103
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-13 00:48:14 -0600

    InterpolacionLineal Codigo Parte 2

--- Tema5_Interpolacion/Interpolacion_Lineal/InterpolacionLineal.java
+++ Tema5_Interpolacion/Interpolacion_Lineal/InterpolacionLineal.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
 public class InterpolacionLineal {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public static double interpolar(double x0, double y0, double x1, double y1, double x) {
+        // Fórmula de interpolación lineal
+        return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
+    }
 }
