7835faf923b7763dae355a4f973ec7c336b13ae7
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-12 23:49:19 -0600

    Eliminacion Documentación del Codigo

--- Tema3_Metodos_para_Sistemas_de_Ecuaciones_Lineales/Codigos_desarrollados_en_clase/EliminacionDEGauss.java
+++ Tema3_Metodos_para_Sistemas_de_Ecuaciones_Lineales/Codigos_desarrollados_en_clase/EliminacionDEGauss.java
@@ -1,49 +1,62 @@
 import java.util.Scanner;
 import java.util.Random;
 
+// Clase que implementa el método de eliminación gaussiana para resolver sistemas de ecuaciones lineales
 public class Eliminacion
 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         
+        // Solicita al usuario el tamaño del sistema (número de ecuaciones y variables)
         System.out.print("Introduce el tamaño de la matriz");
         int orden = sc.nextInt();
         
+        // Genera aleatoriamente una matriz aumentada de tamaño [orden x (orden + 1)]
         double[][] matriz = generarMatriz(orden);
         
+        // Proceso de eliminación hacia adelante
         for(int j = 0; j < orden; j++)
         {
             for(int i = 0; j < orden; i++)
             {
                 if (i > j) 
                 {
+                    // Calcula el factor de eliminación
                     double division = matriz[i][j] / matriz[j][j];
                     System.out.printf("Haciendo cero el elemento en fila %d, columna %d (usando fila %d):\n", i + 1, j + 1, j + 1);
                     System.out.printf("Factor de eliminacion = %.3f\n", division);
+
+                    // Resta la fila j multiplicada por el factor a la fila i
                     for (int k = 0; k < orden + 1; k++) 
                     {
                         matriz[i][k] = matriz[i][k] - division * matriz[j][k];
                     }
+
+                    // Muestra el estado actual de la matriz después de la eliminación
                     mostrarMatriz(matriz, orden);  
                 }
             }    
         }
         
+        // Sustitución hacia atrás para obtener la solución del sistema
         double[] x = new double[orden];
         
         System.out.println("Resolviendo por sustitucion hacia atras:");
         for (int i = orden - 1; i >= 0; i--) 
         {
             double suma = 0;
+            // Suma de los términos ya resueltos
             for (int j = i + 1; j < orden; j++) 
             {
                 suma += matriz[i][j] * x[j];
             }
+
+            // Calcula el valor de la variable actual
             x[i] = (matriz[i][orden] - suma) / matriz[i][i];
             System.out.printf("x%d = %.3f\n", i, x[i]);
         }
-        
+        // Muestra la solución final del sistema
         System.out.println("\nSolucion final:");
         for (int i = 0; i < orden; i++) 
         {
@@ -53,6 +66,12 @@ public class Eliminacion
         sc.close();
     }
     
+     /**
+     * Genera una matriz aumentada aleatoria de tamaño n x (n+1)
+     *
+     * @param orden número de ecuaciones (y variables)
+     * @return matriz aumentada con coeficientes aleatorios entre -5 y 4
+     */
     public static double[][] generarMatriz(int orden)
     {
         Random rand = new Random();
@@ -70,6 +89,12 @@ public class Eliminacion
         
     }
     
+     /**
+     * Muestra por consola la matriz aumentada con formato
+     *
+     * @param matriz matriz aumentada a imprimir
+     * @param orden  número de ecuaciones (filas)
+     */
     public static void mostrarMatriz(double[][] matriz, int orden)
     {
         for (int i = 0; i < orden; i++) 
