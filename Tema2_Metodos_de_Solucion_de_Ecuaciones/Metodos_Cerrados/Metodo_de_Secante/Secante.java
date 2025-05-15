1ce4b806d7745dda27a1f90614ea093a5ec16504
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-14 21:06:51 -0600

    Secante Codigo Parte 2

--- Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Secante/Secante.java
+++ Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Secante/Secante.java
@@ -1,3 +1,13 @@
 public class MetodoSecante {
-    
+
+    public static void main(String[] args) {
+        double x0 = 0.0;         // Primera aproximación inicial
+        double x1 = 1.0;         // Segunda aproximación inicial
+        double tolerancia = 1e-6; // Tolerancia deseada
+        int maxIter = 100;        // Máximo número de iteraciones
+
+        // Llamar al método de la secante
+        secante(x0, x1, tolerancia, maxIter);
+    }
+
 }
