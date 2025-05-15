a5e7b4b71f12e99efcac209fe6466bdeedab6b91
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-14 20:38:34 -0600

    NewtonRaphson Codigo Parte 2

--- Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Newton_Raphson/NewtonRaphson.java
+++ Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Newton_Raphson/NewtonRaphson.java
@@ -1,3 +1,14 @@
 public class MetodoNewtonRaphson {
+
+    public static void main(String[] args) {
+        // Aproximación inicial, tolerancia y número máximo de iteraciones
+        double x0 = 1.5;
+        double tolerancia = 1e-6;
+        int maxIteraciones = 100;
+
+        // Llamar al método de Newton-Raphson
+        newtonRaphson(x0, tolerancia, maxIteraciones);
+    }
+
     
 }
