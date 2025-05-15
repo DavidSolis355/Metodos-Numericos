1d4d1ca74ce7481c934c5a16fba79ace40a51b44
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-14 20:31:23 -0600

    Biseccion Codigo Parte 2

--- Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Abiertos/Metodo_de_Biseccion/Biseccion.java
+++ Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Abiertos/Metodo_de_Biseccion/Biseccion.java
@@ -1,3 +1,13 @@
 public class Biseccion {
 
+    public static void main(String[] args) {
+        // Definir intervalo [a, b], tolerancia y número máximo de iteraciones
+        double a = 1;
+        double b = 2;
+        double tolerancia = 1e-6;
+        int maxIteraciones = 100;
+
+        // Llamar al método de bisección
+        biseccion(a, b, tolerancia, maxIteraciones);
+    }
 }
