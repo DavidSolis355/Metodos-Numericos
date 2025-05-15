f7bbe1a42002274ad410048bfc3041299336b48e
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-14 21:03:38 -0600

    PuntoFijo Codigo Parte 2

--- Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Punto_Fijo/PuntoFijo.java
+++ Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Punto_Fijo/PuntoFijo.java
@@ -1,5 +1,13 @@
 public class MetodoPuntoFijo {
 
-    
+    public static void main(String[] args) {
+        double x0 = 0.5;         // Valor inicial
+        double tolerancia = 1e-6; // Tolerancia deseada
+        int maxIter = 100;        // Número máximo de iteraciones
 
+        // Llamada al método de Punto Fijo
+        puntoFijo(x0, tolerancia, maxIter);
+    }
+
+    
 }
