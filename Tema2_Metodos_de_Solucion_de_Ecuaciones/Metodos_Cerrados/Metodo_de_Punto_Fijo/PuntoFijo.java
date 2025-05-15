dd6b79819cf80b31f139b822835ad7ea6c1cf39c
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-14 21:03:59 -0600

    PuntoFijo Codigo Parte 3

--- Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Punto_Fijo/PuntoFijo.java
+++ Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Cerrados/Metodo_de_Punto_Fijo/PuntoFijo.java
@@ -9,5 +9,14 @@ public class MetodoPuntoFijo {
         puntoFijo(x0, tolerancia, maxIter);
     }
 
-    
+    /**
+     * Función g(x) tal que x = g(x).
+     * Esta función debe cumplir con las condiciones de convergencia del método.
+     * @param x Valor de entrada
+     * @return Resultado de g(x)
+     */
+    public static double g(double x) {
+        // Ejemplo: para f(x) = x^2 - 3x + 2, se puede usar g(x) = (x^2 + 2)/3
+        return (Math.pow(x, 2) + 2) / 3;
+    }
 }
