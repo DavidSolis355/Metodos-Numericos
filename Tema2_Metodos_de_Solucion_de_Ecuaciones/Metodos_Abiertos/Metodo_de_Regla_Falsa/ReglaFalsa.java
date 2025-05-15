2f241c6a5499e6329f136e5a6d027f6095ee189f
DavidSolis355 <23030006@itesa.edu.mx>
2025-05-14 20:35:48 -0600

    ReglaFalsa Codigo Parte 2

--- Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Abiertos/Metodo_de_Regla_Falsa/ReglaFalsa.java
+++ Tema2_Metodos_de_Solucion_de_Ecuaciones/Metodos_Abiertos/Metodo_de_Regla_Falsa/ReglaFalsa.java
@@ -1,4 +1,14 @@
 public class MetodoReglaFalsa {
 
-    
+    public static void main(String[] args) {
+        // Intervalo inicial, tolerancia y máximo de iteraciones
+        double a = 1;
+        double b = 2;
+        double tolerancia = 1e-6;
+        int maxIteraciones = 100;
+
+        // Llamar al método de Regla Falsa
+        reglaFalsa(a, b, tolerancia, maxIteraciones);
+    }
+
 }
